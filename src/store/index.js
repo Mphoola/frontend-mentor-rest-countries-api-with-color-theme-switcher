772713ce8f12bc43d6c1e7ee02ad3f411897c758
mphoola <promisemphoola2@gmail.com>
2023-02-14 01:03:58 -0300

    vuex set

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,31 @@
 import { createStore } from "vuex";
 
 export default createStore({
-    state: {},
-    getters: {},
-    mutations: {},
-    actions: {},
-    modules: {},
+    state: {
+        countries: [],
+        country: {},
+    },
+    getters: {
+        getCountries: (state) => state.countries,
+        getCountry: (state) => state.country,
+    },
+    mutations: {
+        SET_COUNTRIES: (state, countries) => (state.countries = countries),
+        SET_COUNTRY: (state, country) => (state.country = country),
+    },
+    actions: {
+        async fetchCountries({ commit }) {
+            const response = await fetch("https://restcountries.com/v3.1/all");
+            const data = await response.json();
+            commit("SET_COUNTRIES", data);
+        },
+        async fetchCountry({ commit }, name) {
+            const response = await fetch(`https://restcountries.com/v3.1/name/${name}`);
+            const data = await response.json();
+            commit("SET_COUNTRY", data);
+        },
+        
+    },
 });
 
 
