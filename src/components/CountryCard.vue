baed627244d06d02c0cbc232615ba293fd1ede11
mphoola <promisemphoola2@gmail.com>
2023-02-14 00:45:31 -0300

    home page desgin for desktop done

---
+++ src/components/CountryCard.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="rounded-md bg-white">
+        <img :src="flag" alt="">
+        <div class="p-5">
+            <h2 class="font-bold py-2">{{ country }}</h2>
+            <p class="font-semibold">Population: <span class="text-gray-500">{{ population }}</span></p>
+            <p class="font-semibold">Region: <span class="text-gray-500">{{ region }}</span></p>
+            <p class="font-semibold">Capital: <span class="text-gray-500">{{ capital }}</span></p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "CountryCard",
+    props: {
+        flag: {
+            type: String,
+            required: true
+        },
+        country: {
+            type: String,
+            required: true
+        },
+        population: {
+            type: String,
+            required: true
+        },
+        region: {
+            type: String,
+            required: true
+        },
+        capital: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,104 @@
 <template>
-    <div class="text-center">
-        Home
+    <div class="">
+        <Navbar />
+
+        <div class="px-6 pt-4">        
+            <div class="flex justify-between">
+                <!-- search input  -->
+                <div class="relative">
+                    <input type="text" class="border border-gray-300 pl-12 py-2 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" placeholder="Search for a country..." />
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 absolute top-2.5 left-4">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                    </svg>
+                </div>
+
+                <!-- filter by region -->
+                <div class="">
+                    <select class="border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent">
+                        <option value="Filter by Region">Filter by Region</option>
+                        <option value="Africa">Africa</option>
+                        <option value="America">America</option>
+                        <option value="Asia">Asia</option>
+                        <option value="Europe">Europe</option>
+                        <option value="Oceania">Oceania</option>
+                    </select>
+                </div>
+            </div>
+
+            <!-- country cards -->
+            <div class="grid grid-cols-4 gap-6 my-4">
+                <CountryCard 
+                    :flag="'https://flagcdn.com/af.svg'"
+                    :country="'Afghanistan'"
+                    :population="'38,928,346'"
+                    :region="'Asia'"
+                    :capital="'Kabul'"
+                />
+
+                <CountryCard 
+                    :flag="'https://flagcdn.com/al.svg'"
+                    :country="'Albania'"
+                    :population="'2,877,797'"
+                    :region="'Europe'"
+                    :capital="'Tirana'"
+                />
+
+                <CountryCard 
+                    :flag="'https://flagcdn.com/dz.svg'"
+                    :country="'Algeria'"
+                    :population="'43,851,044'"
+                    :region="'Africa'"
+                    :capital="'Algiers'"
+                />
+
+                <CountryCard 
+                    :flag="'https://flagcdn.com/ad.svg'"
+                    :country="'Andorra'"
+                    :population="'77,265'"
+                    :region="'Europe'"
+                    :capital="'Andorra la Vella'"
+
+                />
+
+                <CountryCard 
+                    :flag="'https://flagcdn.com/ao.svg'"
+                    :country="'Angola'"
+                    :population="'32,866,272'"
+                    :region="'Africa'"
+                    :capital="'Luanda'"
+                />
+
+                <CountryCard 
+                    :flag="'https://flagcdn.com/mw.svg'"
+                    :country="'Malawi'"
+                    :population="'18,90238'"
+                    :region="'Africa'"
+                />
+
+                
+                <CountryCard 
+                    :flag="'https://flagcdn.com/ao.svg'"
+                    :country="'Angola'"
+                    :population="'32,866,272'"
+                    :region="'Africa'"
+                    :capital="'Luanda'"
+
+                />
+
+                <CountryCard 
+                    :flag="'https://flagcdn.com/mw.svg'"
+                    :country="'Malawi'"
+                    :population="'18,90238'"
+                    :region="'Africa'"
+                />
+                
+            </div>
+        </div>
     </div>
 </template>
+
+<script setup>
+import Navbar from '../components/Navbar.vue'
+import CountryCard from '../components/CountryCard.vue'
+
+</script>
